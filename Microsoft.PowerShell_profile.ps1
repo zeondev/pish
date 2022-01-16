12a1d9c8c77221315199ef330f90e481e73909fa
Kat21 <49850194+datkat21@users.noreply.github.com>
2022-01-16 02:40:25 -0600

    yes

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,32 +1,35 @@
 # Profile
-clear;
+Clear-Host;
 Write-Host "Welcome to" -NoNewLine;
 Write-Host " Pish v1.4" -ForegroundColor Green;
 $ConfigLoc = "C:\Users\$($env:username)\Documents\WindowsPowerShell\config.json";
 $PkgLoc = "C:\Users\$($env:username)\Documents\WindowsPowerShell\packages";
 $ConfigJson = Get-Content $ConfigLoc | ConvertFrom-Json;
+`
 
 function Get-PishPath {
     $arg = "";
-    if ($args[0] -ne $null) {
+    if ($null -ne $args[0]) {
         $arg = $args[0];
     }
     $loc = $(get-location).Path;
     $loc = $loc -replace "C:\\Users\\$($env:username)", "~";
-    $loc = $loc -replace "\\","/";
-    $loc = $loc -replace "C:/","/";
-    $loc = $loc -replace "^([a-zA-Z]):/","/`$1/";
+    $loc = $loc -replace "\\", "/";
+    $loc = $loc -replace "C:/", "/";
+    $loc = $loc -replace "^([a-zA-Z]):/", "/`$1/";
     $locarr = $loc.Split("/");
     $newloc = @("");
     $index = 0;
     foreach ($item in $locarr) {
         if ($item -ne "") {
-            if ($index -eq $locarr.length-1) {
+            if ($index -eq $locarr.length - 1) {
                 $newloc += $item.ToLower();
-            } else {
+            }
+            else {
                 if ($arg -ne "-Expand") {
                     $newloc += $item.Substring(0, 1).ToLower();
-                } else {
+                }
+                else {
                     $newloc += $item.ToLower();
                 }
             }
@@ -46,15 +49,17 @@ $chars = @{
     "cross" = [regex]::Unescape("\u2717"); # ✗
 }
 
-if ($ConfigJson -ne $null) {
+if ($null -ne $ConfigJson) {
     Write-Host "$($chars.check) Loaded configuration successfully." -ForegroundColor DarkGreen;
     if ($ConfigJson.startdir) {
         try {
             Set-Location $ConfigJson.startdir -ErrorAction SilentlyContinue;
-        } catch {
+        }
+        catch {
             Write-Host "$($chars.cross) Could not change directory to $ConfigJson.startdir.`nPlease read more from the documentation.`n" -ForegroundColor DarkRed;
         }
-    } else {
+    }
+    else {
         Set-Location "C:\Users\$($env:username))";
     }
 } 
@@ -62,18 +67,19 @@ else {
     Write-Host "$($chars.cross) Failed to load configuration.`n" -ForegroundColor DarkRed;
 }
 
-Write-Host "To access our new package manager, type 'pishpack' (without the quotes).`n";
+Write-Host "To access our new package manager, type 'pishpack' (without the quotes).";
 
 function Prompt {
     $name = $env:username.ToLower();
-    $name = $name -Replace " ","";
+    $name = $name -Replace " ", "";
     if ($ConfigJson.user) {
         $name = $ConfigJson.user;
     }
     Write-Host $name -ForegroundColor Green -NoNewLine;
     if ($ConfigJson.host) {
         Write-Host "@$($ConfigJson.host) " -NoNewLine;
-    } else {
+    }
+    else {
         Write-Host "@$($env:computername) " -NoNewLine;
     }
     
@@ -95,7 +101,7 @@ function Prompt {
 }
 
 $PSReadLineOptions = @{
-    Colors = @{
+    Colors             = @{
         # "Command"    = "Blue";
         "Command"            = 'Blue'
         "Number"             = 'Green'
@@ -107,7 +113,7 @@ $PSReadLineOptions = @{
         "ContinuationPrompt" = 'DarkCyan'
         "Default"            = 'Cyan'
     }
-    ContinuationPrompt = "                                  ";
+    ContinuationPrompt = "> ";
 }
 Set-PSReadLineOption @PSReadLineOptions;
 
@@ -117,19 +123,22 @@ function pishpack {
 
     if ($comamnd -eq "install") {
         Write-Host "Going to install $pkg";
-        $pkgurl = wget "https://raw.githubusercontent.com/zeondev/pishpackrepo/main/$($pkg).ps1";
+        $pkgurl = Invoke-WebRequest "https://raw.githubusercontent.com/zeondev/pishpackrepo/main/$($pkg).ps1";
         $pkgcontent = $pkgurl.content
         Set-Content $PkgLoc/$($pkg).ps1 $pkgcontent
         Write-Host "Wrote $($($pkgurl).rawcontentlength) bytes to '$PkgLoc/$($pkg).ps1'`nTry running '$($pkg)' now!";
-    } elseif ($comamnd -eq "uninstall") {
+    }
+    elseif ($comamnd -eq "uninstall") {
         Write-Host "Attempting to uninstall $pkg...";
         if (Test-Path $PkgLoc/$($pkg).ps1) {
             Remove-Item $PkgLoc/$($pkg).ps1 -Force -Recurse -ErrorAction SilentlyContinue;
             Write-Host "Successfully uninstalled $pkg";
-        } else {
+        }
+        else {
             Write-Host "Could not find $pkg to uninstall";
         }
-    } else {
+    }
+    else {
         Write-Host "Invalid argument";
         return;
     }
@@ -141,32 +150,33 @@ $boxes = @{
     # box drawing characters
     "row" = [regex]::Unescape("\u2500"); # 
     "col" = [regex]::Unescape("\u2502"); # │
-    "bl" = [regex]::Unescape("\u2514"); # └
-    "br" = [regex]::Unescape("\u2518"); # ┘
-    "tl" = [regex]::Unescape("\u250C"); # ┌
-    "tr" = [regex]::Unescape("\u2510"); # ┐
-    "h" = [regex]::Unescape("\u2501"); # ━
-    "v" = [regex]::Unescape("\u2503"); # ┃
-    "ul" = [regex]::Unescape("\u2534"); # ┴
-    "ur" = [regex]::Unescape("\u252C"); # ┬
-    "ll" = [regex]::Unescape("\u251C"); # ├
-    "lr" = [regex]::Unescape("\u2524"); # ┤
-    "t" = [regex]::Unescape("\u250F"); # ┏
-    "b" = [regex]::Unescape("\u2513"); # ┓
-    "l" = [regex]::Unescape("\u2517"); # ┗
-    "r" = [regex]::Unescape("\u251B"); # ┛
-    "u" = [regex]::Unescape("\u2533"); # ┻
-    "d" = [regex]::Unescape("\u252B"); # ┫
+    "bl"  = [regex]::Unescape("\u2514"); # └
+    "br"  = [regex]::Unescape("\u2518"); # ┘
+    "tl"  = [regex]::Unescape("\u250C"); # ┌
+    "tr"  = [regex]::Unescape("\u2510"); # ┐
+    "h"   = [regex]::Unescape("\u2501"); # ━
+    "v"   = [regex]::Unescape("\u2503"); # ┃
+    "ul"  = [regex]::Unescape("\u2534"); # ┴
+    "ur"  = [regex]::Unescape("\u252C"); # ┬
+    "ll"  = [regex]::Unescape("\u251C"); # ├
+    "lr"  = [regex]::Unescape("\u2524"); # ┤
+    "t"   = [regex]::Unescape("\u250F"); # ┏
+    "b"   = [regex]::Unescape("\u2513"); # ┓
+    "l"   = [regex]::Unescape("\u2517"); # ┗
+    "r"   = [regex]::Unescape("\u251B"); # ┛
+    "u"   = [regex]::Unescape("\u2533"); # ┻
+    "d"   = [regex]::Unescape("\u252B"); # ┫
 }
 
 function Get-SimpleFileName {
     try {
         Write-Host "  $($boxes.tl)$($boxes.row)$($boxes.row)$($boxes.row) $(Get-PishPath -Expand)" -ForegroundColor Green;
-    } catch {
+    }
+    catch {
         Write-Host "  $($boxes.tl)$($boxes.row)$($boxes.row)$($boxes.row) Unknown directory" -ForegroundColor Green;
     }
     [array]$Files = @(Get-ChildItem);
-    foreach($file in $Files) {
+    foreach ($file in $Files) {
         Write-Host "  $($boxes.col) " -ForegroundColor DarkGreen -NoNewLine;
         Write-Host "$($file.Name)" -ForegroundColor Green;
     }
@@ -177,22 +187,40 @@ function Get-SimpleFileName {
 Remove-Item 'Alias:\ls' -ErrorAction SilentlyContinue -Force;
 Set-Alias -Name "ls" -Value "Get-SimpleFileName";
 # New-Alias "hello world" $PkgLoc"\hello.ps1";
+New-Alias "piwd" "Get-PishPath";
 
-# function to look through $PkgLoc and find all .ps1 files
-# and add them to the alias list for easy access
-$galindex = 0
 function Get-AliasList {
-    $outputDebug = $args[0];
-    $outputDebug2 = $args[1];
-    [array]$Files = @(Get-ChildItem $PkgLoc);
+    try {
+        if (!(Test-Path $PkgLoc)) {
+            $err = $true;
+        }
+        $outputDebug = $args[0];
+        $outputDebug2 = $args[1];
+        [array]$Files = @(Get-ChildItem $PkgLoc -ErrorAction SilentlyContinue);
+    }
+    catch {
+        $err = $true;
+    }
+    if ($err -eq $true -and $outputDebug -eq "-outputdebug" -and $outputDebug2 -eq "yes") {
+        Write-Host "Something went wrong fetching packages to alias, check the directory $PkgLoc" -ForegroundColor Red;
+        # attempt to create the directory
+        if (!(Test-Path $PkgLoc)) {
+            Write-Host "Attempting to create directory $PkgLoc" -ForegroundColor Green;
+            New-Item -Path $PkgLoc -ItemType Directory -Force -ErrorAction SilentlyContinue;
+        }
+        else {
+            Write-Host "Directory $PkgLoc already exists" -ForegroundColor Green;
+        }
+        return;
+    }
     if ($outputDebug -eq "-outputdebug" -and $outputDebug2 -eq "yes") {
         Write-Host "Setting up aliases..." -ForegroundColor Green -NoNewLine;
     }
-    foreach($file in $Files) {
+    foreach ($file in $Files) {
         if ($file.Extension -eq ".ps1") {
             try {
                 $TheAlias = $file.Name;
-                $TheAlias = $TheAlias -Replace ".ps1","";
+                $TheAlias = $TheAlias -Replace ".ps1", "";
                 $ThePath = "$PkgLoc\$($File.Name.ToString())";
                 New-Alias $TheAlias.ToString() $ThePath -Force -Scope Global;
                 if ($outputDebug -eq "-outputdebug" -and $outputDebug2 -eq "yes") {
@@ -207,7 +235,14 @@ function Get-AliasList {
         }
     }
     if ($outputDebug -eq "-outputdebug" -and $outputDebug2 -eq "yes") {
-        Write-Host "Done`n" -ForegroundColor Green -NoNewLine;
+        if ($Files.Length -gt 0) {
+            Write-Host "Done" -ForegroundColor Green -NoNewLine;
+        }
+        else {
+            Write-Host "Couldn't find any.." -ForegroundColor Red -NoNewLine;
+        }
+        Write-Host "`nConsult the documentation to disable this alias message.`n" -ForegroundColor Blue;
+        return;
     }
 }
 Get-AliasList -outputdebug yes;
